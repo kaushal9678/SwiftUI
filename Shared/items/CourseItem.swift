eddcd75f7b7991d49a1a3c2913c8bf21ca1818f1
kaushal9250 <kaushlendra.yadav@careerbuilder.com>
2021-04-17 22:10:50 +0530

    Custom Component or Library creation with Modifiers for SWIFT UI

--- Shared/items/CourseItem.swift
+++ Shared/items/CourseItem.swift
@@ -36,9 +36,7 @@ struct CourseItem: View {
                 
         }
         .padding(.all)
-        .background(course.color)
-        .clipShape(RoundedRectangle(cornerRadius: cornerRadius,style: .continuous))
-        .shadow(color: course.color.opacity(0.3), radius: 20,x: 0,y: 10)
+        .cardStyle()
     }
 }
 
